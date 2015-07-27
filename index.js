153b78c6513aa0b3c05ed96844c76a6e74e794d0
Ondřej Dušek <ondrej.dusek@pasante.cz>
2015-07-27 16:57:03 +0200

    refactor; correct html parsing

--- index.js
+++ index.js
@@ -1,108 +1,123 @@
 'use strict';
 
-var path = require('path'),
-	fs = require('fs'),
-	es = require('event-stream'),
-	gulp = require('gulp'),
-	cheerio = require('cheerio'),
-	sprintf = require('sprintf'),
+var wait = function (stream, callback) {
+	var chunks = [];
 
-	/**
-	 * Zkontroluje pøístup k souboru a vyhodí chybu v pøípadì, že nelze pøeèíst.
-	 * @param file	Název souboru
-	 */
-	validateFile = function (file) {
-		fs.accessSync && fs.accessSync(file, fs.R_OK);
-		if (fs.existsSync && !fs.existsSync(file)) {
-			throw new Error(sprintf('ENOENT, no such file or directory \'%s\'', file));
-		}
-	},
+	stream.on('error', function (err) {
+		callback(error);
+	});
 
-	/**
-	 * Zkonvertuje pole názvù souborù na zdrojový stream Gulpu
-	 * @param cwd	Pracovní adresáø
-	 * @returns {*}
-	 */
-	gulpSources = function (cwd) {
-		cwd = cwd || '';
-		var pathStream = es.through(),
-			fileStream = es.through();
-		pathStream.pipe(es.writeArray(function (err, files) {
-			files = files.map(function (file) {
-				file = path.join(cwd, file);
-				validateFile(file);
-				return file;
+	stream.on('data', function (chunk) {
+		chunks.push(chunk);
+	});
+
+	stream.on('end', function () {
+		var data = chunks.join('');
+		callback(null, data);
+	});
+
+	stream.resume();
+};
+
+var readFile = function (cwd) {
+	var fs = require('fs'),
+		path = require('path'),
+		through2 = require('through2'),
+		File = require('vinyl');
+
+	return through2.obj(function (chunk, end, callback) {
+		var filename = path.join(cwd || '', chunk);
+		fs.readFile(filename, function (err, data) {
+			if (err) {
+				callback(err);
+				return;
+			}
+			var file = new File({
+				path: filename,
+				contents: data
 			});
-			gulp.src(files).pipe(fileStream);
-		}));
-		return es.duplex(pathStream, fileStream);
-	},
+			console.log(file);
+			callback(null, file);
+		});
+	});
+};
 
-	/**
-	 * Builder funkce pro script tag
-	 * @param builder	Builder funkce
-	 * @param minified	Použít minifikovanou verzi sriptu
-	 * @returns {Function}
-	 */
-	scriptBuilder = function (builder, minified) {
-		minified = minified !== false;
-		var extractSrc = function (line) {
-			var element = cheerio('script[src]', line) || cheerio('<script></script>');
-			return minified && element.attr('data-build-src') || element.attr('data-abs-src') || element.attr('src');
-		};
-		return function (block) {
-			var templateScript = function (path) {
-					var element = cheerio('script', '<script></script>');
-					element.attr('src', path);
-					return block.indent + cheerio.html(element);
-				},
-				extractScriptStream = es.mapSync(extractSrc),
-				templateScriptStream = es.mapSync(templateScript);
-			block.pipe(extractScriptStream);
-			templateScriptStream.pipe(block);
-			builder(es.duplex(templateScriptStream, extractScriptStream));
-		};
-	},
+var scriptSrc = function (html, min) {
+	var stream = require('stream'),
+		cheerio = require('cheerio'),
+		readable = new stream.Readable({objectMode: true}),
+		$ = cheerio.load(html);
 
-	/**
-	 * Builder funkce pro link tag
-	 * @param builder	Builder funkce
-	 * @param minified	Použít minifikovanou verzi sriptu
-	 * @returns {Function}
-	 */
-	stylesheetBuilder = function (builder, minified) {
-		minified = minified !== false;
-		var extractLink = function (line) {
-			var element = cheerio('link[rel=stylesheet][href]', line) || cheerio('<link>');
-			return minified && element.attr('data-build-href') ||  element.attr('data-href') || element.attr('href');
-		};
-		return function (block) {
-			var templateLink = function (path) {
-					var element = cheerio('link', '<link>');
-					element.attr('rel', 'stylesheet');
-					element.attr('href', path);
-					return block.indent + cheerio.html(element);
-				},
-				extractLinkStream = es.mapSync(extractLink),
-				templateLinkStream = es.mapSync(templateLink);
-			block.pipe(extractLinkStream);
-			templateLinkStream.pipe(block);
-			builder(es.duplex(templateLinkStream, extractLinkStream));
-		};
-	};
+	$('link[rel=stylesheet][href]').each(function () {
+		var element = $(this),
+			href;
+		if (min) {
+			href = element.attr('min-href') || element.attr('data-min-href');
+		}
+		href = href || element.attr('target-href') || element.attr('data-target-href') || element.attr('href');
+		href && readable.push(href);
+	});
+	readable.push(null);
+	return readable;
+};
+
+var stylesheetSrc = function (html, min) {
+	var stream = require('stream'),
+		cheerio = require('cheerio'),
+		readable = new stream.Readable({objectMode: true}),
+		$ = cheerio.load(html);
+
+	$('script[src]').each(function () {
+		var element = $(this),
+			href;
+		if (min) {
+			href = element.attr('min-src') || element.attr('data-min-src');
+		}
+		href = href || element.attr('target-src') || element.attr('data-target-src') || element.attr('src');
+		href && readable.push(href);
+	});
+	readable.push(null);
+	return readable;
+};
 
 module.exports = {
+
 	/**
 	 * Builder pro vytvoøení streamu se seznamem všech skriptù
 	 * @param stream	Výstupní stream
-	 * @param cwd		Pracovní adresáø
-	 * @param minified	Použít minifikované verze sriptù
+	 * @param cwd	Pracovní adresáø
+	 * @param min	Použít minifikované verze sriptù
 	 * @returns {Function}
 	 */
-	scriptStream: function (stream, cwd, minified) {
-		return scriptBuilder(function (block) {
-			block.pipe(gulpSources(cwd)).pipe(stream);
-		}, minified);
+	streamScript: function (stream, cwd, min) {
+		min = min !== false;
+		return function (block) {
+			wait(block, function (err, html) {
+				if (err) throw err;
+				scriptSrc(html, min)
+					.pipe(readFile(cwd))
+					.pipe(stream);
+			});
+		};
+	},
+
+	/**
+	 * Builder pro vytvoøení streamu se seznamem všech souborù se styly
+	 * @param stream	Výstupní stream
+	 * @param cwd	Pracovní adresáø
+	 * @param min	Použít minifikované verze souborù se styly
+	 * @returns {Function}
+	 */
+	streamStylesheet: function (stream, cwd, min) {
+		min = min !== false;
+		return function (block) {
+			wait(block, function (err, html) {
+				if (err) throw err;
+				stylesheetSrc(html, min)
+					.pipe(readFile(cwd))
+					.pipe(stream);
+			});
+		};
 	},
 
 	/**
@@ -110,23 +125,13 @@ module.exports = {
 	 * @param src	Hodnota src atributu
 	 * @returns {Function}
 	 */
-	scriptReplace: function (src) {
-		return scriptBuilder(function (block) {
-			block.end(src);
-		});
-	},
-
-	/**
-	 * Builder pro vytvoøení streamu se seznamem všech souborù se styly
-	 * @param stream	Výstupní stream
-	 * @param cwd		Pracovní adresáø
-	 * @param minified	Použít minifikované verze souborù se styly
-	 * @returns {Function}
-	 */
-	stylesheetStream: function (stream, cwd, minified) {
-		return stylesheetBuilder(function (block) {
-			block.pipe(gulpSources(cwd)).pipe(stream);
-		}, minified);
+	replaceScript: function (src) {
+		return function (block) {
+			var cheerio = require('cheerio'),
+				element = cheerio('<script></script>');
+			element.attr('src', src);
+			block.end(block.indent + cheerio.html(element));
+		};
 	},
 
 	/**
@@ -134,9 +139,13 @@ module.exports = {
 	 * @param href	Hodnota href atributu
 	 * @returns {Function}
 	 */
-	stylesheetReplace: function (href) {
-		return stylesheetBuilder(function (block) {
-			block.end(href);
-		});
+	replaceStylesheet: function (href) {
+		return function (block) {
+			var cheerio = require('cheerio'),
+				element = cheerio('<link>');
+			element.attr('rel', 'stylesheet');
+			element.attr('href', href);
+			block.end(block.indent + cheerio.html(element));
+		};
 	}
 };
